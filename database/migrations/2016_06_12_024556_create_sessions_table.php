3beac8026c3dfe4826f3581245ba73d05626f816
dougtebay <dougtebay@gmail.com>
2016-06-12 02:34:46 -0400

    Updates

--- database/migrations/2016_06_12_024556_create_sessions_table.php
+++ database/migrations/2016_06_12_024556_create_sessions_table.php
@@ -13,7 +13,7 @@ class CreateSessionsTable extends Migration
     public function up()
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->increments('id');
+            // $table->increments('id');
         });
     }
 
